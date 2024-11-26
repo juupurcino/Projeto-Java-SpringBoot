232e726b2ee42b76fe04919c54702c523ec5cabd
sasa2754 <sasamor5@gmail.com>
2024-11-26 11:31:05 -0300

    create aswer

--- FrontEnd/src/question/scripts/script.js
+++ FrontEnd/src/question/scripts/script.js
@@ -3,14 +3,64 @@ const questionTitle = document.getElementById('titleQuestion');
 const userName = document.getElementById("userName")
 const question = document.getElementById("question")
 const card = document.getElementById("cardQuestion")
+const btnNewAnswer = document.getElementById('newAnswer');
 
 const urlParams = new URLSearchParams(window.location.search);
 const questionId = urlParams.get('idQuestion');
+const spaceId = urlParams.get('idSpace');
+
+btnNewAnswer.addEventListener('click', () => {
+
+    let token = localStorage.getItem('token')
+
+    if (!token) {
+        console.log("Token nâo encontrado, permissão negada!");
+        return;
+    }
+
+    token = token.replace(/\\/g, '');
+    token = token.replace(/^"(.*)"$/, '$1');
+    token = token.replace(/^"(.*)"$/, '$1');
+
+    const btnFechar = document.getElementById("btnFechar");
+    const answer = document.getElementById('answerText').value;
+    console.log(answer);
+
+    const idSpace = spaceId;
+    const idQuestion = questionId;
+    const answerData = {answer, idQuestion, idSpace};
+
+
+    fetch(`http://localhost:8080/answer`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(answerData),
+        credentials: 'include'
+    })
+    .then(response => {
+        if (!response.ok) {
+            alert("Você não pode responder essa pergunta!");
+            throw new Error('Erro na requisição!');
+        }
+        console.log("Resposta criada!")
+        btnFechar.click();
+        location.reload();
+        return response.json();
+    })
+
+    .catch(error => {
+        console.log("Error: ", error);
+    })
+})
 
 window.onload = () => {
 
-    if (questionId) {
+    if (questionId && spaceId) {
         console.log(`ID da questão: ${questionId}`);
+        console.log(`ID do Espaço: ${spaceId}`);
         getQuestionInfo(questionId);
         getAnswers(questionId)
   
