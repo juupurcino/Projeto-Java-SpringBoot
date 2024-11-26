13bbf91249f57e2778f1e11e84bf735325916550
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-11-26 08:54:16 -0300

    pegando informaçoes do espaço

--- FrontEnd/src/members/scripts/addMembers.js
+++ FrontEnd/src/members/scripts/addMembers.js
@@ -8,7 +8,7 @@ window.onload = () => {
     if (spaceId) {
         console.log(`ID do espaço: ${spaceId}`);
         getSpaceInfo(spaceId);
-        getQuestionBySpace(spaceId);
+        // getQuestionBySpace(spaceId);
         // fetchQuestions(spaceId);
     } else {
         console.log("ID não encontrado na URL.");
@@ -30,10 +30,6 @@ function getSpaceInfo(spaceId) {
 
     var idNumber = parseInt(spaceId, 10);
 
-    const viewMembersButton = document.querySelector('.btn-secondary a');
-
-    viewMembersButton.href = `/FrontEnd/src/members/index.html?idSpace=${idNumber}`;
-
     fetch(`http://localhost:8080/spaces/${idNumber}`, {
         method: 'GET',
         headers: {
@@ -54,7 +50,9 @@ function getSpaceInfo(spaceId) {
     })
 }
 
-function getQuestionBySpace(spaceId) {
+
+
+function getMenbersBySpace(spaceId) {
     let token = localStorage.getItem('token')
 
     if (!token) {
@@ -107,67 +105,3 @@ function getQuestionBySpace(spaceId) {
         }
     })
 }
-
-async function deleteQuestion(idQuestion) {
-    
-    let token = localStorage.getItem('token')
-
-    if (!token) {
-        console.log("Token nâo encontrado, permissão negada!");
-        return;
-    }
-
-    token = token.replace(/\\/g, '');
-    token = token.replace(/^"(.*)"$/, '$1');
-    token = token.replace(/^"(.*)"$/, '$1');
-
-
-    fetch(`http://localhost:8080/question/${idQuestion}`, {
-        method: 'DELETE',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        }
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erro na requisição');
-        }
-        // Converta o corpo da resposta para JSON
-        return response.json();
-    })
-}
-
-
-
-// Função para buscar as perguntas com base no ID do espaço
-// function fetchQuestions(spaceId) {
-//     // Exemplo de chamada para uma API (substitua com a URL da sua API ou lógica de banco de dados)
-//     fetch(`/api/perguntas?${spaceId}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log("Perguntas do espaço:", data);
-//             displayQuestions(data);
-//         })
-//         .catch(error => console.error("Erro ao carregar perguntas:", error));
-// }
-
-// Função para renderizar as perguntas no HTML
-// function displayQuestions(questions) {
-//     const cardContainer = document.getElementById('cardContainer');
-//     cardContainer.innerHTML = ''; // Limpa qualquer conteúdo existente
-    
-//     questions.forEach(question => {
-//         const card = document.createElement('div');
-//         card.classList.add('card', 'w-75', 'h-25');
-//         card.innerHTML = `
-//             <div class="card-body">
-//                 <button class="btn btn-danger btnExcluir" style="position: absolute; right: 5%;">🗑️</button>
-//                 <h5 class="card-title">${question.title}</h5>
-//                 <p class="card-text">${question.body}</p>
-//                 <p class="card-text"><small class="text-muted">Atualizado por último há ${question.timeAgo}</small></p>
-//             </div>
-//         `;
-//         cardContainer.appendChild(card);
-//     });
-// }
